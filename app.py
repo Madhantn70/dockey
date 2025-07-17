0b6176dfe055de14fd096a35ab0d69cc5c6a795e
Madhan <madhantn70@gmail.com>
2025-07-17 15:21:38 +0530

    Bind Flask to 0.0.0.0 and use PORT env var for Render deployment

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ def delete_submission():
     return redirect(url_for('admin_dashboard'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
